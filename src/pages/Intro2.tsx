940aa4b5be37c9e675fad32aa764ec3d28890b33
skqjatn6788@gmail.com <123361606+Bemosu@users.noreply.github.com>
2025-02-17 22:33:40 +0900

    페이지 레이아웃 개선
    개선중

--- src/pages/Intro2.tsx
+++ src/pages/Intro2.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import goto from '../../public/gotoconsult.png'
+
 
 const Intro2 = () => {
   return (
-    <div>
-        <img src='../../public/gotoconsult.png' alt='consultbutton'/>
-    </div>
+<div className="relative flex flex-col gap-8 items-center justify-center min-h-screen p-8 bg-cover bg-center"
+style={{ backgroundImage: `url(${goto.src})` }}>
+  </div>
   )
 }
 
