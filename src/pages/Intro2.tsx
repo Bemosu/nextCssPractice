91ebe964fe6bacd5eabc1c8f252e35dec8e79801
skqjatn6788@gmail.com <123361606+Bemosu@users.noreply.github.com>
2025-02-18 00:38:14 +0900

    인트로 디자인 조정중
    화면비율 방법 수정중

--- src/pages/Intro2.tsx
+++ src/pages/Intro2.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import goto from '../../public/gotoconsult.png'
+import Image from 'next/image'
 
 
 const Intro2 = () => {
   return (
-<div className="relative flex flex-col gap-8 items-center justify-center min-h-screen p-8 bg-cover bg-center"
-style={{ backgroundImage: `url(${goto.src})` }}>
+<main className="relative flex flex-col items-center justify-center min-h-10 px-12">
+ <div className='absolute left-0 top-0 z-10 flex h-full w-full justify-between '>
+  <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
+  <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
+  <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
+  <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
+  <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
+ </div>
+  <div className='flex justify-center items-center z-20 '>
+  <div className="w-full max-w-[600px] mt-24">
+    <Image src="/gotoconsult.png" alt="gotoconsult" layout="responsive" width={16} height={9} />
   </div>
+  </div>
+  </main>
   )
 }
 
