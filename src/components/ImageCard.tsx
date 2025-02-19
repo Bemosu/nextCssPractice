2549d57d53a7191afd19256fecf140b0cd684f8a
skqjatn6788@gmail.com <123361606+Bemosu@users.noreply.github.com>
2025-02-19 21:54:20 +0900

    css라이브러리 수정
    image 카드 제작 중

---
+++ src/components/ImageCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ImageCard = () => {
+  return (
+    <div className='w-[460px] h-[460px] bg-gray-400'></div>
+  )
+}
+
+export default ImageCard
--- src/pages/Intro2.tsx
+++ src/pages/Intro2.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
+import ImageCard from '@/components/ImageCard'
 
 
 const Intro2 = () => {
   return (
-<main className="relative flex flex-col items-center justify-center min-h-10 px-12">
+<main className="relative flex flex-col items-center justify-center min-h-10 px-5">
  <div className='absolute left-0 top-0 z-10 flex h-full w-full justify-between '>
   <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
   <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
@@ -12,11 +13,14 @@ const Intro2 = () => {
   <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
   <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
  </div>
-  <div className='flex justify-center items-center z-20 '>
+ <div className='flex flex-col justify-center items-center z-20 '>
   <div className="w-full max-w-[600px] mt-24">
     <Image src="/gotoconsult.png" alt="gotoconsult" layout="responsive" width={16} height={9} />
   </div>
-  </div>
+ </div>
+ <div>
+ <ImageCard/>
+ </div>
   </main>
   )
 }
