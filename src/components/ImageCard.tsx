61b7531a76229c5184c4b0d21936f930482380ab
skqjatn6788@gmail.com <123361606+Bemosu@users.noreply.github.com>
2025-02-27 23:28:42 +0900

    이미지 카드 컴포넌트
    props를 이용해서 재사용이 가능하도록 제작

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { forwardRef} from 'react'
+import Image from "next/image";
 
-const ImageCard = () => {
-  return (
-    <div className='w-[340px] h-[340px] bg-gray-400'></div>
-  )
+interface ImageCardProps {
+  src : string;
+  alt? : string;
+  type? : "image" | "video" | "svg";
 }
 
-export default ImageCard
+const ImageCard = forwardRef<HTMLDivElement, ImageCardProps>(({src, alt = "media", type = "image"},ref) => {
+  return (
+    <div ref={ref} className='w-[340px] h-[340px] bg-gray-400'>
+      {type == "image" && <Image src={src} alt={alt} width={340} height={340} className="object-cover w-full h-full"></Image>}
+      {type == "video" && (
+        <video autoPlay loop muted className='w-full h-full object-cover'>
+          <source src={src} type='video/mp4'></source> 
+        </video>)}
+      {type === "svg" && <img src={src} alt={alt} className="w-full h-full object-contain" />}
+    </div>
+  );
+}
+);
+ImageCard.displayName = "ImageCard";
+
+export default ImageCard;
--- src/pages/Intro2.tsx
+++ src/pages/Intro2.tsx
@@ -7,7 +7,7 @@ import NameBox from '@/components/NameBox'
 const Intro2 = () => {
   return (
 <main className="relative flex flex-col items-center justify-center min-h-10 ">
- <div className='absolute left-0 top-0 z-10 flex h-full w-full justify-between p-[3%] '>
+ <div className='absolute left-0 top-0 z-10 flex h-full w-full justify-between px-80 '>
   <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
   <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
   <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
@@ -15,7 +15,7 @@ const Intro2 = () => {
   <div className='h-full w-0 border-l-[1px] border-l-[#383738]'/>
  </div>
  <div className='flex flex-col justify-center items-center z-20 '>
-  <div className="w-full max-w-[600px] mt-24">
+  <div className="w-full max-w-[800px] mt-24">
     <Image src="/gotoconsult.png" alt="gotoconsult" layout="responsive" width={16} height={9} />
   </div>
   <div className='flex flex-row justify-center gap-[30%] z-20'>
